4d9b788a663b13ef7b648fe507a921433252363d
nyowusu <yaw.owusu@hotmail.com>
2019-08-07 08:33:31 +0100

    Updates

---
+++ precache-manifest.cc6788ff9ad0b7b1ff7fd6d52f325081.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40bfe04fd2c8d14a3fe80b9a166e30d4",
+    "url": "/agenda/index.html"
+  },
+  {
+    "revision": "c0e9750ad52b5659b5fc",
+    "url": "/agenda/static/css/main.e7b063e5.chunk.css"
+  },
+  {
+    "revision": "f0b72de02b702cb4fcc0",
+    "url": "/agenda/static/js/2.5cb38976.chunk.js"
+  },
+  {
+    "revision": "c0e9750ad52b5659b5fc",
+    "url": "/agenda/static/js/main.93c31cf2.chunk.js"
+  },
+  {
+    "revision": "bb30883cb00aecbee018",
+    "url": "/agenda/static/js/runtime~main.e3891923.js"
+  }
+]);
